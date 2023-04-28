82c46a372dbd63adcd369d5a4620214cd52a30ea
PaosLuna <lunapp20@gmail.com>
2023-04-28 16:17:59 -0600

    Data1, pagina 1 lista

--- src/main.js
+++ src/main.js
@@ -1,6 +1,25 @@
 import { example } from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
-
+//import data from './data/pokemon/pokemon.js';
+import data from './data/rickandmorty/rickandmorty.js';
 console.log(example, data);
+
+
+//AQUÍ EMPIEZA NUESTRO CÓDIGO
+const seccion1 = document.getElementById("container");
+const seccion2 = document.getElementById("seccion2");
+seccion2.style.display = 'none';
+
+const botonTodos = document.getElementById("botonTodos");
+botonTodos.addEventListener("click", ()=> {
+    //cerrarSeccion1();
+    abrirSeccion2();
+});
+
+/*function cerrarSeccion1(){
+    seccion1.style.display = 'none';
+}*/
+
+function abrirSeccion2(){
+    seccion2.style.display = 'block'; 
+}

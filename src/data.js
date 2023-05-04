e4974d376efaeb5e3a157731f919fb7acf2bfec4
PaosLuna <lunapp20@gmail.com>
2023-05-04 16:55:39 -0600

    Ya se ve la data en la sección 2

--- src/data.js
+++ src/data.js
@@ -1,9 +1,27 @@
+import data from './data/rickandmorty/rickandmorty.js';
 // estas funciones son de ejemplo
 
-export const example = () => {
-  return 'example';
+console.log("hola");
+
+
+const filtrarNombres = (dataName) => {
+  let name = data.results.filter((e) => e.name === dataName);
+  console.log(name);
 };
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+export {filtrarNombres};
+
+
+
+
+/*/FILTRA POR NOMBRE 
+function mostrarNombres(){
+  const personajes = data.results;
+  console.log(personajes);
+  for (let i=0; i<personajes.length; i++){    
+  personajes[i].name;
+  let traerPersonajes = personajes[i].name;
+  }
+}*/
+
+
--- src/main.js
+++ src/main.js
@@ -1,25 +1,61 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-//import data from './data/pokemon/pokemon.js';
 import data from './data/rickandmorty/rickandmorty.js';
-console.log(example, data);
+console.log(data.results);
+import {filtrarNombres} from './data.js';
 
+console.log(filtrarNombres);
 
 //AQUÍ EMPIEZA NUESTRO CÓDIGO
 const seccion1 = document.getElementById("container");
 const seccion2 = document.getElementById("seccion2");
+const espacioPersonajes = document.getElementById("personajes");
 seccion2.style.display = 'none';
 
+//CIERRA SECCIÓN 1 Y ABRE LA 2
 const botonTodos = document.getElementById("botonTodos");
 botonTodos.addEventListener("click", ()=> {
-    //cerrarSeccion1();
-    abrirSeccion2();
+    cerrarSeccion1();
+    mostrarTodo();
 });
 
-/*function cerrarSeccion1(){
+function cerrarSeccion1(){
     seccion1.style.display = 'none';
-}*/
-
-function abrirSeccion2(){
     seccion2.style.display = 'block'; 
 }
+
+//CIERRA SECCIÓN 2 Y ABRE LA 1
+const botonInicio = document.getElementById("botonInicio");
+botonInicio.addEventListener("click", ()=> {
+    cerrarSeccion2();
+});
+
+function cerrarSeccion2(){
+    seccion2.style.display = 'none';
+    seccion1.style.display = 'block'; 
+}
+
+//MOSTRAR TODOS LO PERSONAJES SIN FILTRO 
+function mostrarTodo(){
+    for (let todos of data.results){
+        console.log(todos);
+        espacioPersonajes.innerHTML += '<div class="tarjetaP">' + '<img class="imagenTarjeta" src="' + todos.image + '">' + '<br>' + '<p class="textoTarjeta">' + "Nombre: " + todos.name + '<br>'  + "Estatus: " + todos.status + '<br>' + "Especie: " + todos.species + '<br>' + "Tipo: " + todos.type + '<br>' + "Genero: " + todos.gender + '<br>' + "Origen: " + todos.origin.name + '<br>' + "Locación: " + todos.location.name + '</p>' + '</div>';
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

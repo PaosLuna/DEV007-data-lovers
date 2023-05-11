b7246e05ada4a47690773fa2dcb6e4a8cdd5ea48
PaosLuna <lunapp20@gmail.com>
2023-05-11 10:10:39 -0600

    Ya hay sección mutiversos

--- src/data.js
+++ src/data.js
@@ -1,30 +1,25 @@
 import data from './data/rickandmorty/rickandmorty.js';
 // estas funciones son de ejemplo
-console.log("hola");
-
-const filtrarNombres = (dataName) => {
+//console.log("hola");
+/*const filtrarNombres = (dataName) => {
   let name = data.results.filter((e) => e.name === dataName);
   console.log(name);
 };
+export {filtrarNombres};*/
 
-export {filtrarNombres};
-
-
-//BUSCADOR BUENO YA FUNCIONA, FAVOR DE NO MOVER NADA 
-const buscadorDos = document.getElementById("buscadorDos");
+//BUSCADOR1 BUENO YA FUNCIONA, FAVOR DE NO MOVER NADA
+const buscador1 = document.getElementById("buscador1");
 const resultado = document.querySelector('#resultado');
 const espacioPersonajes = document.getElementById("personajes");
-
-const filtrar = () => {
+const filtrar1 = () => {
     resultado.innerHTML = '';
-    const texto = buscadorDos.value.toLowerCase();
+    const texto = buscador1.value.toLowerCase();
     for (let personaje of data.results) {
         let nombre = personaje.name.toLowerCase();
         if (nombre.indexOf(texto) !== -1){
-            resultado.innerHTML += ` 
+            resultado.innerHTML += `
             <div class="tarjetaP">${'<img class="imagenTarjeta" src="' + personaje.image + '">' + '<br>' + '<p class="textoTarjeta">' + personaje.name + '<br>' + "Estatus: " + personaje.status + '<br>' + "Especie: " + personaje.species + '<br>' + "Tipo: " + personaje.type + '<br>' + "Genero: " + personaje.gender + '<br>' + "Origen: " + personaje.origin.name + '<br>' + "Locación: " + personaje.location.name + '</p>'}</div>
-            ` 
-            
+            `
             espacioPersonajes.style.display = 'none';
         }
     }
@@ -35,22 +30,66 @@ const filtrar = () => {
     }
 }
 
-export {filtrar};
+//BUSCADOR2 BUENO YA FUNCIONA, FAVOR DE NO MOVER NADA
+const buscadorDos = document.getElementById("buscadorDos");
+const filtrar = () => {
+    resultado.innerHTML = '';
+    const texto = buscadorDos.value.toLowerCase();
+    for (let personaje of data.results) {
+        let nombre = personaje.name.toLowerCase();
+        if (nombre.indexOf(texto) !== -1){
+            resultado.innerHTML += `
+            <div class="tarjetaP">${'<img class="imagenTarjeta" src="' + personaje.image + '">' + '<br>' + '<p class="textoTarjeta">' + personaje.name + '<br>' + "Estatus: " + personaje.status + '<br>' + "Especie: " + personaje.species + '<br>' + "Tipo: " + personaje.type + '<br>' + "Genero: " + personaje.gender + '<br>' + "Origen: " + personaje.origin.name + '<br>' + "Locación: " + personaje.location.name + '</p>'}</div>
+            `
+            espacioPersonajes.style.display = 'none';
+        }
+    }
+    if(resultado.innerHTML === ''){
+        resultado.innerHTML += `
+        <li>Personaje no encontrado</li>
+        `
+    }
+}
+
+//FILTRO DE LETRAS
+
+let isalfabeto = (orden) => {
+let dataAlfabeto = data.results.slice();
+dataAlfabeto.sort((a,b) =>{
+const nombreA = a.name.toLowerCase();
+const nombreB = b.name.toLowerCase();
+if(nombreA<nombreB){
+  console.log(dataAlfabeto);
+  return -1
+}
+if(nombreA>nombreB){
+  return 1
+} else {
+  const inverso = dataAlfabeto.reverse();
+  console.log(inverso);
+}
+return dataAlfabeto;
+})
+}
 
 
+export {filtrar1, filtrar, isalfabeto};
 
 
-
-
-
-/*/FILTRA POR NOMBRE 
-function mostrarNombres(){
-  const personajes = data.results;
-  console.log(personajes);
-  for (let i=0; i<personajes.length; i++){    
-  personajes[i].name;
-  let traerPersonajes = personajes[i].name;
+//FILTRA POR MULTIVERSOS
+function mostrarMultiversos(){
+  const multiversos = data.results;
+  for (let i=0; i<multiversos.length; i++){
+  multiversos[i].origin.name;
+  let traerMultiversos = multiversos[i].origin.name;
+  console.log(traerMultiversos);
   }
-}*/
+}
+mostrarMultiversos();
+
+
+
+
+
 
 
--- src/main.js
+++ src/main.js
@@ -1,14 +1,17 @@
 import data from './data/rickandmorty/rickandmorty.js';
 console.log(data.results);
-import {filtrar} from './data.js';
-
-
+import {filtrar1, filtrar, isalfabeto} from './data.js';
 
 //AQUÍ EMPIEZA NUESTRO CÓDIGO
 const seccion1 = document.getElementById("container");
 const seccion2 = document.getElementById("seccion2");
+const seccion3 = document.getElementById("seccion3");
+const header1 = document.getElementById("header1");
+const header2 = document.getElementById("header2");
 const espacioPersonajes = document.getElementById("personajes");
 seccion2.style.display = 'none';
+seccion3.style.display = 'none';
+header2.style.display = 'none';
 
 //CIERRA SECCIÓN 1 Y ABRE LA 2
 const botonTodos = document.getElementById("botonTodos");
@@ -16,10 +19,12 @@ botonTodos.addEventListener("click", ()=> {
     cerrarSeccion1();
     mostrarTodo();
 });
-
 function cerrarSeccion1(){
     seccion1.style.display = 'none';
-    seccion2.style.display = 'block'; 
+    seccion2.style.display = 'block';
+    seccion3.style.display = 'none';
+    header2.style.display = 'block';
+    header1.style.display = 'none';
 }
 
 //CIERRA SECCIÓN 2 Y ABRE LA 1
@@ -30,11 +35,42 @@ botonInicio.addEventListener("click", ()=> {
 
 function cerrarSeccion2(){
     seccion2.style.display = 'none';
-    seccion1.style.display = 'flex'; 
-
+    seccion1.style.display = 'flex';
+    seccion3.style.display = 'none';
+    header1.style.display = 'flex';
+    header2.style.display = 'none';
 }
 
-//MOSTRAR TODOS LO PERSONAJES SIN FILTRO 
+//CIERRA SECCIÓN 1 Y ABRE LA 3
+const botonMultiversos = document.getElementById("botonMultiversos");
+botonMultiversos.addEventListener("click", ()=> {
+    abrirSeccion3();
+    mostrarTodo();
+});
+function abrirSeccion3(){
+    seccion1.style.display = 'none';
+    seccion2.style.display = 'none';
+    seccion3.style.display = 'block';
+    header2.style.display = 'block';
+    header1.style.display = 'none';
+}
+
+//CIERRA SECCIÓN 3 Y ABRE LA 1
+const botonInicio2 = document.getElementById("botonInicio2");
+botonInicio2.addEventListener("click", ()=> {
+    cerrarSeccion3();
+});
+
+function cerrarSeccion3(){
+    seccion2.style.display = 'none';
+    seccion1.style.display = 'flex';
+    seccion3.style.display = 'none';
+    header1.style.display = 'flex';
+    header2.style.display = 'none';
+}
+
+
+//MOSTRAR TODOS LOS PERSONAJES SIN FILTRO
 function mostrarTodo(){
     for (let todos of data.results){
         console.log(todos);
@@ -43,24 +79,55 @@ function mostrarTodo(){
 }
 
 
+//LO QUE HICIMOS CON VIVIE
+let orden = "AZ";
+
+function mostrarFiltrado(orden){
+    let dataFiltrado = isalfabeto(orden);
+    for (let todos of dataFiltrado){
+        console.log(todos);
+        espacioPersonajes.innerHTML += '<div class="tarjetaP" id="tarjetaP">' + '<img class="imagenTarjeta" src="' + todos.image + '">' + '<br>' + '<p class="textoTarjeta">' + "Nombre: " + todos.name + '<br>'  + "Estatus: " + todos.status + '<br>' + "Especie: " + todos.species + '<br>' + "Tipo: " + todos.type + '<br>' + "Genero: " + todos.gender + '<br>' + "Origen: " + todos.origin.name + '<br>' + "Locación: " + todos.location.name + '</p>' + '</div>';
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
 const botonBuscador = document.getElementById("botonBuscador");
 botonBuscador.addEventListener("click", filtrar)
+const botonBuscar = document.getElementById("botonBuscar");
+botonBuscar.addEventListener ("click", event => {
+    //event.preventDefault;
+    filtrar1();
+    cerrarSeccion1();
+})
 
-console.log("commitPrueba2");
 
+//ESTO ES DEL SELECTOR POR ALFABETO INTENTO 1
+const selectorAbc = document.getElementById("selector1")
+//document.getElementById(dataAlfabeto).
+selector1.addEventListener("change", function(){
+    
+  const seleccionado = selector1.value;
+  //console.log(isalfabeto);
+  mostrarFiltrado(seleccionado);
+})
 
 
 
 //ESTO ES DEL BUSCADOR INTENTO 1
 //let buscadorDos = document.getElementById("buscadorDos");
 //let tarjetaP = document.getElementById("tarjetaP");
-
-//buscadorFiltro(buscadorDos, tarjetaP); 
+//buscadorFiltro(buscadorDos, tarjetaP);
 //ESTO ES DEL BUSCADOR INTENTO 1
-
-
-
-
 
 
 

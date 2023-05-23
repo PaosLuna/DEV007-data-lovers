59354dfc7cfeb1c4580791821222e9dc74a81bb9
PaosLuna <lunapp20@gmail.com>
2023-05-23 14:18:52 -0600

    iniciando test

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,25 +1,62 @@
-/*import data from './data/rickandmorty/rickandmorty.js';*/
-import { filtrar1, /*filtrar2, isalfabeto, filtrarorigen, episodiosTotal*/ } from '../src/data.js';
+import data from '../src/data/rickandmorty/rickandmorty.js';
+import { filtrar1, filtrar2, isalfabeto, filtrarorigen, episodiosTotal} from '../src/data.js';
+
 
 
 describe('filtar1', () => {
   it('is a function', () => {
     expect(typeof filtrar1).toBe('function');
   });
-});
-/*it('returns `example`', () => {
-    expect(example()).toBe('example');
-  });
-});
 
 
-describe('anotherExample', () => {
+  it('retorna un array vacio cuando el texto no se encuentra', () => {
+    const resultado = filtrar1("Paola")
+    expect(resultado.length).toBe(0)
+  })
+
+  it('filtrar por Bepisian', () => {
+    const bepisian = filtrar1("bepisian")
+    expect(bepisian.length).toBe(1)
+  })
+});
+
+describe('filtar2', () => {
   it('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
+    expect(typeof filtrar1).toBe('function');
   });
 
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
+
+  it('retorna un array vacio cuando el texto no se encuentra', () => {
+    const resultado = filtrar2("Paola")
+    expect(resultado.length).toBe(0)
+  })
+
+  it('filtrar por Bepisian', () => {
+    const bepisian = filtrar2("bepisian")
+    expect(bepisian.length).toBe(1)
+  })
+});
+
+
+describe('isalfabeto', () => {
+  it('is a function', () => {
+    expect(typeof isalfabeto).toBe('function');
+  });
+
+  it('Se espera que retorne un array', () => {
+    /*const resultado = isalfabeto("ascendente")*/
+    expect(typeof isalfabeto(data)).toBe('array');
   });
 });
-*/
+
+describe('filtrarorigen', () => {
+  it('is a function', () => {
+    expect(typeof filtrarorigen).toBe('function');
+  });
+});
+
+describe('episodiosTotal', () => {
+  it('is a function', () => {
+    expect(typeof episodiosTotal).toBe('object');
+  });
+});
